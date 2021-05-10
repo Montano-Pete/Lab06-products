2d8f93b110603f6a88c8d0a437082d0999005707
Peter <pete225848@gmail.com>
2021-05-10 13:14:52 -0700

    Established HTML and first product

---
+++ fruits.js
@@ -0,0 +1,8 @@
+const cherry = {
+    id: 'cherry',
+    name: 'Rainier Cherry',
+    description: 'Sweet with a thin skin and thick creamy-yellow flesh!',
+    category: 'tree-fruit',
+    price: 1,
+    cost: 0.25
+};
